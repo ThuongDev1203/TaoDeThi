4d1b3881b3d859223084b5edd3f27567909a7317
ThuongDev123 <cuong.0907062600@gmail.com>
2024-07-11 20:59:12 +0700

    Add project files.

---
+++ TaoDeThi/DangNhap.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TaoDeThi
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Username = new Label();
+            Password = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // Username
+            // 
+            Username.AutoSize = true;
+            Username.Location = new Point(214, 116);
+            Username.Name = "Username";
+            Username.Size = new Size(75, 20);
+            Username.TabIndex = 0;
+            Username.Text = "Username";
+            Username.Click += label1_Click;
+            // 
+            // Password
+            // 
+            Password.AutoSize = true;
+            Password.Location = new Point(214, 190);
+            Password.Name = "Password";
+            Password.Size = new Size(70, 20);
+            Password.TabIndex = 1;
+            Password.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(361, 113);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(233, 27);
+            txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(361, 190);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(233, 27);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(345, 282);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Đăng nhập";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // DangNhap
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(Password);
+            Controls.Add(Username);
+            Name = "DangNhap";
+            Text = "DangNhap";
+            Load += DangNhap_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Username;
+        private Label Password;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+    }
+}
---
+++ TaoDeThi/DangNhap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaoDeThi
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Thay thế các giá trị này bằng thông tin đăng nhập thực tế của bạn
+            string validUsername = "user";
+            string validPassword = "password";
+
+            if (username == validUsername && password == validPassword)
+            {
+                MessageBox.Show("Login successful!");
+                // Mở form chính hoặc thực hiện hành động khác ở đây
+                this.Hide(); // Ẩn form đăng nhập
+                DangNhap dangNhap = new DangNhap(); // Tạo form chính
+                dangNhap.Show(); // Hiển thị form chính
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
